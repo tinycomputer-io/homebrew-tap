462bb4f74ab0c0ef1c2c4ddd86a73b509f0ae524
gautham-v <114679795+gautham-v@users.noreply.github.com>
2026-08-31 12:13:09 -0700

    tinynote 0.4.1

--- Formula/tinynote.rb
+++ Formula/tinynote.rb
@@ -1,8 +1,8 @@
 class Tinynote < Formula
   desc "Minimal, local-first markdown notes TUI over plain files"
   homepage "https://github.com/tinycomputer-io/tinynote"
-  url "https://github.com/tinycomputer-io/tinynote/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "c347f7ba6473cbf58717472ef71a08c7979c1b04bfda9b8f61d3b1cca06634ea"
+  url "https://github.com/tinycomputer-io/tinynote/archive/refs/tags/v0.4.1.tar.gz"
+  sha256 "9b73758a272b79ead538d102129db9360e5ad8308edcb693f85c02c3929c77b4"
   license "MIT"
   head "https://github.com/tinycomputer-io/tinynote.git", branch: "main"
 
